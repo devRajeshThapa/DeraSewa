7a173331dbbbb0e0884b2e983913f4f5313fbf3d
Rajesh Thapa <devrajeshthapa@gmal.com>
2024-07-23 17:10:13 +0545

    Client: Stable program

--- client/App.js
+++ client/App.js
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './Stacks/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{title: 'Welcome'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
